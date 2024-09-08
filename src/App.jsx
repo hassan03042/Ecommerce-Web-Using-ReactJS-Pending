b6cd9032873c8a87b384aadfab152cbb18b36e6c
hassan03042 <muhammadhassan6948@gmail.com>
2024-09-08 23:38:38 +0500

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllProducts } from "../utils/products";
 import Card from "./components/Card";
-import { data } from "autoprefixer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [showCartItems, setShowCartItems] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -24,38 +25,65 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log("cartitems=>", cartItems);
+    const itemsInCart = JSON.parse(localStorage.getItem("cart"));
+    if (itemsInCart) {
+      setCartItems([...itemsInCart]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (cartItems.length) {
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+    }
   }, [cartItems]);
 
   const addCartItems = (item) => {
     const items = [...cartItems];
     const itemInd = items.findIndex((data) => data.id === item.id);
-    if (itemInd == -1) {
-       items.push(item)
-    setCartItems([...items]);
+    if (itemInd === -1) {
+      items.push(item);
+      setCartItems([...items]);
     }
-   
   };
+
+  const iterateOn = showCartItems ? cartItems : products;
 
   return (
     <div className="container mx-auto my-10">
       <div className="fixed w-full bg-white h-[60px] top-0 flex items-center justify-center gap-10">
         <h1 className="text-center text-3xl font-semibold">Shopping List</h1>
-        <h1 className="text-center text-2xl font-semibold underline">
-          Cart Items {cartItems.length}
+        <h1
+          onClick={() => setShowCartItems(!showCartItems)}
+          className="text-center text-2xl font-semibold underline"
+        >
+          {showCartItems
+            ? "Show All Products"
+            : `Cart Items ${cartItems.length}`}
         </h1>
       </div>
       {error && <p className="text-red-500 text-center">{error}</p>}
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {products.map((item) => (
-              <Card
-                addToCart={() => addCartItems(data)}
-                key={item.id}
-                item={item}
-              />
-            ))}
+            {iterateOn.map((item) => {
+              const isAddedToCart =
+                cartItems.findIndex((product) => product.id === item.id) !== -1;
+              return (
+                <Card
+                  addToCart={() => addCartItems(item)}
+                  key={item.id}
+                  item={item}
+                  showRemoveFromCart={showCartItems === true}
+                  isAddedToCart={isAddedToCart}
+                  removeFromCart={() => {
+                    const allOtherItem = cartItems.filter(
+                      (product) => product.id !== item.id
+                    );
+                    setCartItems([...allOtherItem]);
+                  }}
+                />
+              );
+            })}
           </div>
         </div>
       </section>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,10 @@
-function Card({ item, addToCart }) {
+function Card({
+  item,
+  addToCart,
+  isAddedToCart,
+  showRemoveFromCart,
+  removeFromCart,
+}) {
   const { id, category, images, description, title, price } = item;
 
   // Skip rendering if the ID is 6
@@ -13,7 +19,7 @@ function Card({ item, addToCart }) {
     <div className="p-4 md:w-1/3">
       <div className="flex flex-col h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img
-          className={`w-full object-cover object-center bg-gray-400`}
+          className={`w-full object-cover object-center bg-gray-300`}
           src={`${images[0]}`}
           alt="blog"
         />
@@ -24,27 +30,25 @@ function Card({ item, addToCart }) {
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {title}
           </h1>
-          <p className="leading-relaxed mb-3 flex-grow">
-            {description}
-          </p>
-          <div 
-          onClick={addToCart}
-          className="flex items-center flex-wrap mt-auto cursor-pointer">
-            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-              Add To Cart
-              <svg
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M5 12h14" />
-                <path d="M12 5l7 7-7 7" />
-              </svg>
+          <p className="leading-relaxed mb-3 flex-grow">{description}</p>
+          <div
+            onClick={addToCart}
+            className="flex items-center flex-wrap mt-auto cursor-pointer"
+          >
+            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 text-center">
+              {isAddedToCart ? "Added" : " Add To Cart"}
             </a>
+
+            {showRemoveFromCart && (
+              <div
+                onClick={removeFromCart}
+                className="flex items-center flex-wrap mt-auto cursor-pointer"
+              >
+                <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 mx-20 text-center">
+                  {"Remove From Cart"}
+                </a>
+              </div>
+            )}
           </div>
         </div>
       </div>
