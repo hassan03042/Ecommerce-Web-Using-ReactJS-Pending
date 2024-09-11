864efb90e7b4a94b1b11b67b4d3f209dfb885793
hassan03042 <muhammadhassan6948@gmail.com>
2024-09-11 23:24:23 +0500

    ...

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -18,22 +18,19 @@ function Card({
   return (
     <div className="p-4 md:w-1/3">
       <div className="flex flex-col h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        <img
+        {/* <img
           className={`w-full object-cover object-center bg-gray-300`}
           src={`${images[0]}`}
           alt="blog"
-        />
-        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-          {title}
-        </h1>
-        {/* <div className="flex flex-col p-6 flex-grow">
+        /> */}
+        <div className="flex flex-col p-6 flex-grow">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             {category.name}
           </h2>
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {title}
           </h1>
-          <p className="leading-relaxed mb-3 flex-grow">{description}</p>
+          {/* <p className="leading-relaxed mb-3 flex-grow">{description}</p>
           <div
             onClick={addToCart}
             className="flex items-center flex-wrap mt-auto cursor-pointer"
@@ -52,8 +49,8 @@ function Card({
                 </a>
               </div>
             )}
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </div>
     </div>
   );
