d8d5a44802bf117b9e436e8fd54b54a607e235e3
hassan03042 <muhammadhassan6948@gmail.com>
2024-09-11 23:32:57 +0500

    error fix....

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,29 +8,32 @@ function Card({
   const { id, category, images, description, title, price } = item;
 
   // Skip rendering if the ID is 6
-  //   if (id === 6) {
-  //   return null;
-  // }
+    if (id === 6) {
+    return null;
+  }
 
   // // Define a style for the specific image ID
-  // const imageStyle = id === 6 ? "h-64" : "h-50";
+  const imageStyle = id === 6 ? "h-64" : "h-50";
 
   return (
     <div className="p-4 md:w-1/3">
-      <div className="flex flex-col h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        {/* <img
+      <section className="flex flex-col h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+        <img
           className={`w-full object-cover object-center bg-gray-300`}
           src={`${images[0]}`}
           alt="blog"
-        /> */}
+        />
+        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+          {title}
+        </h1>
         <div className="flex flex-col p-6 flex-grow">
-          {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             {category.name}
-          </h2> */}
+          </h2>
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {title}
           </h1>
-          {/* <p className="leading-relaxed mb-3 flex-grow">{description}</p>
+          <p className="leading-relaxed mb-3 flex-grow">{description}</p>
           <div
             onClick={addToCart}
             className="flex items-center flex-wrap mt-auto cursor-pointer"
@@ -49,9 +52,9 @@ function Card({
                 </a>
               </div>
             )}
-          </div> */}
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
